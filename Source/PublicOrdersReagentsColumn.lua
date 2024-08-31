5b81e6ffe0702f48121d2af5864e3c79fa2447cd
Teelo <49792789+teelolws@users.noreply.github.com>
2024-09-01 09:29:14 +1200

    Repurpose addon: shows reward icons for NPC orders without needing to mouse over

--- Source/PublicOrdersReagentsColumn.lua
+++ Source/PublicOrdersReagentsColumn.lua
@@ -1,3 +1,38 @@
--- This addon became obsolete in 11.0.2 and should be uninstalled!
-
-if true then return end
+EventUtil.ContinueOnAddOnLoaded("Blizzard_Professions", function()
+    hooksecurefunc(ProfessionsFrame.OrdersPage, "ShowGeneric", function(self, orders, browseType, offset, isSorted)
+        if not C_CraftingOrders.ShouldShowCraftingOrderTab() then
+            return
+        end
+        if ProfessionsFrame:GetTab() ~= ProfessionsFrame.craftingOrdersTabID then
+            return
+        end
+        
+        if browseType ~= 1 then return end
+        if self.orderType ~= 3 then return end
+        
+        local rows = self.BrowseFrame.OrderList.ScrollBox:GetView().frames
+        
+        for rowID, row in ipairs(rows) do
+            local cell = row.cells[3]
+            if not cell.RewardIcon then return end
+            local rowData = cell.rowData.option.npcOrderRewards
+            for idx, reward in ipairs(rowData) do
+                local quantity = reward.count
+                local itemLink = reward.itemLink
+                
+                if not cell["RewardIcon"..(idx+1)] then
+                    cell["RewardIcon"..(idx+1)] = CreateFrame("ItemButton", nil, cell, "ProfessionsCrafterOrderRewardTemplate")
+                end
+                
+                local button = cell["RewardIcon"..(idx+1)]
+                button:SetScale(0.5)
+                if idx == 1 then
+                    button:SetPoint("TOPRIGHT", cell.RewardIcon, "TOPLEFT")
+                else
+                    button:SetPoint("TOPRIGHT", cell["RewardIcon"..idx], "TOPLEFT")
+                end
+                button:SetReward(reward)
+            end
+        end
+    end)
+end)
