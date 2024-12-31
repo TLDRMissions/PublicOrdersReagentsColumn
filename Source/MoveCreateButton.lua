57cc5c67edef4127faec369b890cc592eeb19b04
Teelo <49792789+teelolws@users.noreply.github.com>
2024-12-31 13:34:50 +1300

    Update MoveCreateButton.lua

--- Source/MoveCreateButton.lua
+++ Source/MoveCreateButton.lua
@@ -1,15 +1,45 @@
 local addonName, addon = ...
 
 local wasEnabled
-ProfessionsFrame.OrdersPage.OrderView:HookScript("OnShow", function()
+ProfessionsFrame.OrdersPage.OrderView:HookScript("OnShow", function(self)
     if addon.db.global.moveCreateButton then
-        ProfessionsFrame.OrdersPage.OrderView.CreateButton:ClearAllPoints()
-        ProfessionsFrame.OrdersPage.OrderView.CreateButton:SetPoint("TOP", ProfessionsFrame.OrdersPage.OrderView.OrderInfo.StartOrderButton, "TOP")
-        ProfessionsFrame.OrdersPage.OrderView.CreateButton:SetFrameStrata("HIGH")
+        self.CreateButton:ClearAllPoints()
+        self.CreateButton:SetPoint("TOP", self.OrderInfo.StartOrderButton, "TOP")
+        self.CreateButton:SetFrameStrata("HIGH")
+        
+        self.CompleteOrderButton:ClearAllPoints()
+        self.CompleteOrderButton:SetPoint("TOP", self.OrderInfo.StartOrderButton, "TOP")
+        self.CompleteOrderButton:SetFrameStrata("HIGH")
+        
+        self.OrderInfo.DeclineOrderButton:ClearAllPoints()
+        self.OrderInfo.DeclineOrderButton:SetPoint("TOP", self.OrderInfo.ReleaseOrderButton, "TOP")
+        
+        self.OrderInfo.StartOrderButton:ClearAllPoints()
+        self.OrderInfo.StartOrderButton:SetPoint("BOTTOM", self.OrderInfo, "BOTTOM", 0, 55)
+        
+        self.OrderInfo.OrderReagentsWarning:ClearAllPoints();
+	    self.OrderInfo.OrderReagentsWarning:SetPoint("BOTTOMLEFT", self.OrderInfo, "BOTTOMLEFT", 20, 90)
+        
+        self.StartRecraftButton:ClearAllPoints()
+        self.StartRecraftButton:SetPoint("TOP", self.OrderInfo.ReleaseOrderButton, "TOP")
+        self.StartRecraftButton:SetFrameStrata("HIGH")
+        
         wasEnabled = true
     elseif wasEnabled then
-        ProfessionsFrame.OrdersPage.OrderView.CreateButton:ClearAllPoints()
-        ProfessionsFrame.OrdersPage.OrderView.CreateButton:SetPoint("BOTTOMRIGHT", ProfessionsFrame.OrdersPage.OrderView, "BOTTOMRIGHT", -20, 7)
-        ProfessionsFrame.OrdersPage.OrderView.CreateButton:SetFrameStrata("MEDIUM")
+        self.CreateButton:ClearAllPoints()
+        self.CreateButton:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", -20, 7)
+        
+        self.CompleteOrderButton:ClearAllPoints()
+        self.CompleteOrderButton:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", -20, 7)
+        
+        self.OrderInfo.DeclineOrderButton:ClearAllPoints()
+        self.OrderInfo.DeclineOrderButton:SetPoint("BOTTOM", self.OrderInfo, "BOTTOM", 70, 8)
+        
+        -- StartOrderButton and OrderReagentsWarning are anchored dynamically in ProfessionsCrafterOrderViewMixin:SetOrderState
+        
+        self.StartRecraftButton:ClearAllPoints()
+        self.StartRecraftButton:SetPoint("RIGHT", self.CompleteOrderButton, "LEFT", -5, 0)
+
+        wasEnabled = false
     end
 end)
