2116ba0c70148409abcdd00de9555bf38ac74c79
Teelo <49792789+teelolws@users.noreply.github.com>
2025-01-20 15:59:23 +1300

    Implement russian translations
    From: https://github.com/Hubbotu/No-Mats-No-Make/blob/main/ToolSelectionLocales.lua

--- Source/ToolSelectionLocales.lua
+++ Source/ToolSelectionLocales.lua
@@ -16,6 +16,16 @@ L["DISABLE_BUTTON_TEXT"] = "Disable auto-equipping"
 L["PRIORTY_BUTTON_SELECTED_TEXT"] = "Ingenuity over Multicraft"
 L["PRIORTY_BUTTON_DESELECTED_TEXT"] = "Multicraft over Ingenuity"
 
-if GetLocale() == "esES" then
-    -- L["TOOLTOP_EMPTY_BUTTON"] = ""
+if GetLocale() == "ruRU" then
+    L["TOOLTIP_EMPTY_BUTTON"] = "Перетащите сюда инструмент вашей профессии %s, чтобы заменить его при необходимости."
+    L["TOOLTIP_RESOURCEFULNESS_DETAIL"] = "Этот инструмент будет заменен на рабочие заказы и рецепты, не использующие мультикрафт."
+    L["TOOLTIP_MULTICRAFT_DETAIL"] = "Этот инструмент будет заменен на рецепты, использующие мультикрафт, если только это не заказ на работу."
+    L["TOOLTIP_INGENUITY_DETAIL"] = "Этот инструмент будет заменен, если вы выберете опцию использования концентрации."
+    L["TOOLTIP_SPEED_DETAIL"] = "Этот инструмент будет заменен, если рецепт не использует никаких других характеристик."
+    L["TOOLTIP_FINAL_DETAIL"] = "Нажмите, чтобы очистить этот инструмент. Затем он будет пропущен для инструментов с более низким приоритетом."
+    L["TOOLTIP_ERROR_NOT_FOUND"] = "Ошибка: Не удалось найти инструмент назначенной профессии."
+    L["PRIORITY_BUTTON_TOOLTIP"] = "Если выбрано, ваш назначенный инструмент Изобретательности будет использоваться при выборе Концентрации, даже если рецепт использует Мультикрафт"
+    L["DISABLE_BUTTON_TEXT"] = "Отключить автоматическое оснащение"
+    L["PRIORTY_BUTTON_SELECTED_TEXT"] = "Изобретательность в отношении мультикрафта"
+    L["PRIORTY_BUTTON_DESELECTED_TEXT"] = "Мультикрафт важнее изобретательности"
 end
